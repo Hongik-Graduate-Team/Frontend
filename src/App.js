e968ba32a4f655f860483d19ce3b003c03004a2b
Ooorami <90558692+Ooorami@users.noreply.github.com>
2024-07-23 17:01:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import HomePage from './components/pages/Homepage/HomePage';
 import InputInfo from './components/pages/Inputinfo/InputInfo';
 import MyPage from './components/pages/MyPage/MyPage';
 import './index.css';
-import Redirection from "./components/pages/SignInPage/Redirection";
 
 // Kakao SDK 초기화
 window.Kakao.init('YOUR_KAKAO_JAVASCRIPT_KEY');
@@ -22,7 +21,6 @@ function App() {
                     <Route path="/signup" element={<SignUpPage />} />
                     <Route path="/inputinfo" element={<InputInfo />} />
                     <Route path="/mypage" element={<MyPage />} />
-                    <Route path="/redirect" element={<Redirection />} /> {/* 새로 추가된 라우트 */}
                 </Routes>
             </Router>
         </AuthProvider>
