88d928d1be87d14d4c117d33b316681e96b5fe50
Ooorami <wkddbstj8534@gmail.com>
2024-07-23 17:48:01 +0900

    카카오 로그인 재수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './services/AuthContext'; // AuthProvider 가져오기
-import SignInPage from './components/pages/SignInPage/SignInPage';
+import SignInPage from './components/pages/SignInPage';
 import SignUpPage from './components/pages/SignUpPage';
 import HomePage from './components/pages/Homepage/HomePage';
 import InputInfo from './components/pages/Inputinfo/InputInfo';
 import MyPage from './components/pages/MyPage/MyPage';
 import './index.css';
-import Redirection from "./components/pages/SignInPage/Redirection";
 
 // Kakao SDK 초기화
 window.Kakao.init('YOUR_KAKAO_JAVASCRIPT_KEY');
@@ -22,7 +21,6 @@ function App() {
                     <Route path="/signup" element={<SignUpPage />} />
                     <Route path="/inputinfo" element={<InputInfo />} />
                     <Route path="/mypage" element={<MyPage />} />
-                    <Route path="/redirect" element={<Redirection />} /> {/* 새로 추가된 라우트 */}
                 </Routes>
             </Router>
         </AuthProvider>
