825e42288bff70033c06631030a78ddeccc65de2
Ooorami <wkddbstj8534@gmail.com>
2024-08-18 15:37:56 +0900

    카카오 로그인페이지 수정 및 홈페이지 헤더 수정

--- src/services/AuthContext.js
+++ src/services/AuthContext.js
@@ -9,12 +9,21 @@ export const AuthProvider = ({ children }) => {
 
     // 로그인 상태를 확인하는 함수 (실제 로그인 확인 로직으로 대체)
     useEffect(() => {
-        const checkLoginStatus = async () => {
-            // 실제 로그인 확인 로직으로 대체
-            const loggedIn = !!localStorage.getItem('userToken');
-            setIsLoggedIn(loggedIn);
+        const checkLoginStatus = () => {
+            const token = localStorage.getItem('userToken');
+            setIsLoggedIn(!!token);
         };
+
+        // 컴포넌트가 마운트될 때 로그인 상태 확인
         checkLoginStatus();
+
+        // 로그인 상태를 지속적으로 확인하기 위해 'storage' 이벤트를 리스닝
+        window.addEventListener('storage', checkLoginStatus);
+
+        // 컴포넌트가 언마운트될 때 리스너 제거
+        return () => {
+            window.removeEventListener('storage', checkLoginStatus);
+        };
     }, []);
 
     return (
