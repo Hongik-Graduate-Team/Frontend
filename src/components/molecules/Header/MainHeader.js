d5d4f481f3707d95032209b17ae1b2dc3135c9e5
ahnus <103306888+ahnus@users.noreply.github.com>
2024-08-21 21:44:59 +0900

    경로 수정

--- src/components/molecules/Header/MainHeader.js
+++ src/components/molecules/Header/MainHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../../../assets/img/Logo.png';
-import { AuthContext } from '../../services/AuthContext';
+import { AuthContext } from '../../../services/AuthContext';
 
 function MainHeader() {
     const { setIsLoggedIn } = useContext(AuthContext);
