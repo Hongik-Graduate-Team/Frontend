41dfac4404f4daf03434bf61444196b7ddb4be4f
Ooorami <wkddbstj8534@gmail.com>
2024-08-24 10:22:54 +0900

    면접 준비 페이지 배포 에러 수정 시도

--- src/components/pages/Interview/InterviewPreparationPage.js
+++ src/components/pages/Interview/InterviewPreparationPage.js
@@ -27,8 +27,9 @@ const InterviewPreparationPage = () => {
                 });
 
                 const videoStream = new MediaStream(stream.getVideoTracks());
-                if (videoRef.current) {
-                    const videoElement = videoRef.current;
+                const videoElement = videoRef.current;
+
+                if (videoElement) {
                     videoElement.srcObject = videoStream;
 
                     videoElement.onloadedmetadata = () => {
@@ -112,7 +113,107 @@ const InterviewPreparationPage = () => {
                 videoElement.srcObject = null;
             }
         };
-    }, []);
+    }, [videoRef]); // videoRef를 의존성 배열에 추가
+
+    // useEffect(() => {
+    //     const setupMediaDevices = async () => {
+    //         try {
+    //             const stream = await navigator.mediaDevices.getUserMedia({
+    //                 video: true,
+    //                 audio: {
+    //                     echoCancellation: true,
+    //                     noiseSuppression: true,
+    //                     autoGainControl: true,
+    //                 }
+    //             });
+    //
+    //             const videoStream = new MediaStream(stream.getVideoTracks());
+    //             if (videoRef.current) {
+    //                 const videoElement = videoRef.current;
+    //                 videoElement.srcObject = videoStream;
+    //
+    //                 videoElement.onloadedmetadata = () => {
+    //                     videoElement.play().catch(error => console.error('Video play error:', error));
+    //
+    //                     // 모델 로드 후 detectFace 호출
+    //                     loadFaceApiModel();
+    //                 };
+    //             }
+    //
+    //             setupAudio(stream); // 오디오 설정
+    //         } catch (error) {
+    //             console.error('Error accessing media devices.', error);
+    //             alert('카메라와 마이크 권한이 필요합니다.');
+    //         }
+    //     };
+    //
+    //     const setupAudio = (stream) => {
+    //         const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    //         const analyser = audioContext.createAnalyser();
+    //         const microphone = audioContext.createMediaStreamSource(stream);
+    //         microphone.connect(analyser);
+    //
+    //         analyser.fftSize = 512;
+    //         const bufferLength = analyser.frequencyBinCount;
+    //         const dataArray = new Uint8Array(bufferLength);
+    //
+    //         const detectVoice = () => {
+    //             analyser.getByteFrequencyData(dataArray);
+    //             const sum = dataArray.reduce((a, b) => a + b, 0);
+    //             const average = sum / dataArray.length;
+    //
+    //             if (average > 30) {
+    //                 setVoiceDetected(true);
+    //             } else {
+    //                 setVoiceDetected(false);
+    //             }
+    //
+    //             requestAnimationFrame(detectVoice);
+    //         };
+    //
+    //         detectVoice();
+    //     };
+    //
+    //     const loadFaceApiModel = async () => {
+    //         // TinyFaceDetector 모델 로드
+    //         await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+    //
+    //         // 모델 로드 후 얼굴 인식 시작
+    //         detectFace();
+    //     };
+    //
+    //     const detectFace = async () => {
+    //         const videoElement = videoRef.current;
+    //
+    //         const detect = async () => {
+    //             if (videoElement && videoElement.readyState >= 2) {
+    //                 const detections = await faceapi.detectAllFaces(videoElement, new faceapi.TinyFaceDetectorOptions());
+    //
+    //                 if (detections.length > 0) {
+    //                     setFaceDetected(true);
+    //                 } else {
+    //                     setFaceDetected(false);
+    //                 }
+    //             }
+    //
+    //             requestAnimationFrame(detect);
+    //         };
+    //
+    //         detect();
+    //     };
+    //
+    //     setupMediaDevices();
+    //
+    //     return () => {
+    //         const videoElement = videoRef.current;
+    //         if (videoElement && videoElement.srcObject) {
+    //             const stream = videoElement.srcObject;
+    //             const tracks = stream.getTracks();
+    //             tracks.forEach(track => track.stop());
+    //             videoElement.srcObject = null;
+    //         }
+    //     };
+    // }, []);
 
     return (
         <div className="flex flex-col h-screen">
