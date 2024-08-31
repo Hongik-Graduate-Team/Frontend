88db3a67ca38a5e566739c21497232005259f1e4
ahnus <103306888+ahnus@users.noreply.github.com>
2024-08-31 22:42:05 +0900

    0831-3

--- src/components/pages/Inputinfo/InputInfo.js
+++ src/components/pages/Inputinfo/InputInfo.js
@@ -50,6 +50,7 @@ function InputInfo() {
         });
         const data = response.data;
 
+      if (data) {
         setResumeData({
           position: data.position || '',
           questions: data.resumes || [{ resumeId: null, question: '', answer: '' }],
@@ -61,20 +62,8 @@ function InputInfo() {
           certifications: data.certifications || [{ certId: null, certType: '', certDate: null }],
           languageCerts: data.languageCerts || [{ languageCertId: null, languageCertType: '', languageCertLevel: '', languageCertDate: null }]
         });
-
-              // 업데이트된 resumeData를 콘솔에 출력
-      console.log('Updated resumeData:', {
-        position: data.position || '',
-        questions: data.resumes || [{ resumeId: null, question: '', answer: '' }],
-        majors: data.majors || [{ majorId: null, majorName: '' }],
-        gpas: data.gpas || { score: '', total: '' },
-        careers: data.careers || [{ careerId: null, careerType: '', content: '', startDate: null, endDate: null }],
-        stacks: data.stacks || [{ stackId: null, stackLanguage: '', stackLevel: '' }],
-        awards: data.awards || [{ awardId: null, awardType: '', awardPrize: '' }],
-        certifications: data.certifications || [{ certId: null, certType: '', certDate: null }],
-        languageCerts: data.languageCerts || [{ languageCertId: null, languageCertType: '', languageCertLevel: '', languageCertDate: null }]
-      });
-
+      }
+      
       } catch (error) {
         console.error('데이터를 불러오는데 실패했습니다:', error);
       }
@@ -82,6 +71,10 @@ function InputInfo() {
 
     loadData();
   }, []);
+
+  useEffect(() => {
+    console.log('Updated resumeData:', resumeData);
+  }, [resumeData]);
 
   // 페이지 이동 시 경고창 표시
   useEffect(() => {
