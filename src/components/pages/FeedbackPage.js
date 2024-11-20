fad5c430c89b32accc277454c9cdfc74cbb99177
ahnus <103306888+ahnus@users.noreply.github.com>
2024-11-20 21:52:09 +0900

    닉네임 추가

--- src/components/pages/FeedbackPage.js
+++ src/components/pages/FeedbackPage.js
@@ -237,7 +237,7 @@ const FeedbackPage = () => {
       <div className="flex flex-col items-center justify-center">
         <div className="bg-gray-50 shadow-md p-8 mt-4 w-full max-w-7xl">
           <h1 className="text-3xl font-semibold text-indigo-600 mb-8">
-            {nickname ? `${nickname}님의 면접 분석 결과입니다.` : '회원님의 면접 분석 결과입니다.'}
+            {nickname}님의 면접 분석 결과입니다.
           </h1>
 
           <div className="flex flex-cols-1 md:grid-cols-2 gap-8 w-full">
